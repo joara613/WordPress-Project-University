18b4d50552c50ee7b509ec41ad9e593d8a835ab2
joara613 <joara613@gmail.com>
2022-11-22 10:30:31 -0700

    fix minor flaws on search

--- wp-content/themes/fictional-university/include/search-route.php
+++ wp-content/themes/fictional-university/include/search-route.php
@@ -12,7 +12,8 @@ function universityRegisterSearch(){
 function universitySearchResults($data){
 	$mainQuery = new WP_Query(array(
 		'post_type' => array('post', 'page', 'professor', 'program', 'event'),
-		's' => sanitize_text_field($data['term'])
+		's' => sanitize_text_field($data['term']),
+		'posts_per_page' => -1
 	));
 
 	$results = array(
@@ -74,32 +75,56 @@ function universitySearchResults($data){
 		
 	}
 
-	$programsMetaQuery = array('relation' => 'OR');
-	foreach($results['programs'] as $item){
-		array_push($programsMetaQuery, array(
-			'key' =>'related_programs',
-			'compare' => 'LIKE',
-			'value' => '"' . $item['id'] . '"'
-		));
-	}
+	if($results['programs']){
 
-	$programRelationshipQuery = new WP_Query(array(
-		'post_type' => 'professor',
-		'meta_query' => $programsMetaQuery
-		));
+		$programsMetaQuery = array('relation' => 'OR');
+		foreach($results['programs'] as $item){
+			array_push($programsMetaQuery, array(
+				'key' =>'related_programs',
+				'compare' => 'LIKE',
+				'value' => '"' . $item['id'] . '"'
+			));
+		}
+
+		$programRelationshipQuery = new WP_Query(array(
+			'post_type' => array('event','professor'),
+			'meta_query' => $programsMetaQuery
+			));
 
 		while($programRelationshipQuery ->have_posts()){
 			$programRelationshipQuery ->the_post();
 
+			if(get_post_type()=='event'){
+				$eventDate = new DateTime(get_field('event_date'));
+				$excerpt = null;
+				if(has_excerpt()) {
+					$excerpt= get_the_excerpt();
+				}else{
+					$excerpt= wp_trim_words(get_the_content(), 18);
+				}
+
+				array_push($results['events'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink(),
+				'month' => $eventDate -> format('M'),
+				'day' => $eventDate -> format('d'),
+				'excerpt' => $excerpt
+				));
+			}
+
 			if(get_post_type()=='professor'){
-			array_push($results['professors'], array(
-			'title' => get_the_title(),
-			'permalink' => get_the_permalink(),
-			'image'=> get_the_post_thumbnail_url(0,'professorLandscape')
-		));
+				array_push($results['professors'], array(
+				'title' => get_the_title(),
+				'permalink' => get_the_permalink(),
+				'image'=> get_the_post_thumbnail_url(0,'professorLandscape')
+			));
+			}
 		}
+		$results["professors"] = array_values(array_unique($results['professors'],SORT_REGULAR));
+		$results["events"] = array_values(array_unique($results['events'],SORT_REGULAR));
 		}
-	$results["professors"] = array_values(array_unique($results['professors'],SORT_REGULAR));
+
+	
 
 	return $results;
 }
